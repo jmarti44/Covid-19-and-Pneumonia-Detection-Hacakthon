a97e092d5ff5ca8abecbe0fe4bf318686c3d1cd2
jmarti44 <jmarti1997.jm@gmail.com>
2021-11-20 10:21:31 -0600

    upload

--- flask_tool.py
+++ flask_tool.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
 from flask import render_template
-from flask import request
+from flask.scaffold import _matching_loader_thinks_module_is_package
+from werkzeug.utils import secure_filename
+import os
+
+
+
+
 # from werkzeug.wrappers import request
 
 
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/Users/josemartinez/Desktop/Hackathon/images'
+# app.config['SECRET_KEY'] = random_key
 
 @app.route('/')
 def index():
@@ -14,17 +23,25 @@ def index():
 @app.route('/doctor.html')
 def doctor():
     name = request.args.get('name')
-    return render_template('doctor.html')
+    return render_template('doctor.html',name = name)
 
 @app.route('/patient.html')
 def patient():
     return render_template("patient.html")
 
-@app.route('/test.html')
-def upload():
-    return render_template('test.html')
+# @app.route('/test.html')
+# def upload():
+#     return render_template('test.html')
 
-@app.route('/image.html', methods=["POST", "GET"])
-def success():
-    if request.method == 'POST':
-        return render_template('image.html')
+# @app.route('/doctor.html', methods=['GET','POST'])
+# def doctor():
+#     return render_template('doctor.html')
+
+@app.route('/upload.html',methods = ['GET','POST'])
+def upload():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    filename = "covidTest.png"
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+    return "hello"
+
