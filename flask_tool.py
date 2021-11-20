4b3273ed837463ff8b4b93157a6706d6305b0ca7
carissarpatton <crp016@uark.edu>
2021-11-20 13:14:54 -0600

    front end updates

--- flask_tool.py
+++ flask_tool.py
@@ -5,9 +5,6 @@ from flask.scaffold import _matching_loader_thinks_module_is_package
 from werkzeug.utils import secure_filename
 import os
 
-
-
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'images'
 # app.config['SECRET_KEY'] = random_key
@@ -36,22 +33,30 @@ def doctor():
 
     name = request.args.get('name')
     # if request.method == "POST":
-    normal_convert = data["assemble"][0]["normal"]
-    covid_convert = data["assemble"][0]["covid"]
-    pneumonia_convert = data["assemble"][0]["pneumonia"]
-    return render_template('/doctor.html', name = name, normal=normal_convert, covid=covid_convert, pneumonia=pneumonia_convert)
+    normal_convert = 0
+    covid_convert = 0
+    pneumonia_convert = 0
+    return render_template('/doctor.html', name = name, normal= normal_convert, covid=covid_convert, pneumonia=pneumonia_convert)
 
 
 @app.route('/patient.html')
 def patient():
-    return render_template("patient.html")
+    name = request.args.get('name')
+    normal_convert = (data["assemble"][0]["normal"])*100
+    covid_convert = (data["assemble"][0]["covid"])*100
+    pneumonia_convert = (data["assemble"][0]["pneumonia"])*100
+    return render_template('/patient.html', name = name, normal= normal_convert, covid=covid_convert, pneumonia=pneumonia_convert)
 
 
-@app.route('/upload.html',methods = ['GET','POST'])
+@app.route('/image.html',methods = ['GET','POST'])
 def upload():
+    name = request.args.get('name')
     file = request.files['file']
     filename = secure_filename(file.filename)
     filename = "covidTest.png"
     file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-    return "File Uploaded Successfully"
+    normal_convert = (data["assemble"][0]["normal"])*100
+    covid_convert = (data["assemble"][0]["covid"])*100
+    pneumonia_convert = (data["assemble"][0]["pneumonia"])*100
+    return render_template('/patient.html', normal= normal_convert, covid=covid_convert, pneumonia=pneumonia_convert)
 
--- prediction.py
+++ prediction.py
@@ -35,7 +35,7 @@ def generate_Prediction(path):
     return prediction
     
 
-path = "/Users/steveliang/Desktop/Hack/Hackathon/images/covidTest.png"    
+path = "images/covidTest.png" 
 result = generate_Prediction(path)
 
 
