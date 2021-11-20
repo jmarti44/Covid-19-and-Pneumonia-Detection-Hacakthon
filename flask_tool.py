e942a094b5810713b41436bf88e9e40e8950bb52
jmarti44 <jmarti1997.jm@gmail.com>
2021-11-20 07:39:30 -0600

    updated index

--- flask_tool.py
+++ flask_tool.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #testing = "landing"
-    return render_template('test.html')
+    return render_template('index.html')
 
 @app.route('/doctor.html')
 def doctor():
