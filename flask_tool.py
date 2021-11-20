7cd8ad38290c1592851b888dd5ca1b184b79c356
bitcoder-17 <70048387+bitcoder-17@users.noreply.github.com>
2021-11-20 01:42:43 -0600

    test

--- flask_tool.py
+++ flask_tool.py
@@ -5,8 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def doctor():
-    testing = "landing page"
+def index():
+    testing = "landing"
     return testing
 
 @app.route('/doctor.html')
