90194616542debe31c6e928895be4e1f9b6d1307
bitcoder-17 <70048387+bitcoder-17@users.noreply.github.com>
2021-11-20 01:32:36 -0600

    update

--- flask_tool.py
+++ flask_tool.py
@@ -1,10 +1,10 @@
 from flask import Flask
 import json
-from flask import *
-#from script_python import kernel as K
+
 
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     testing = "testing from flask to vuejs"
